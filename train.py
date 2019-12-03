8d5842937ee560e56244d94d27a7b29977fa56f6
anjalipemmaraju <anjali.pemmaraju@gmail.com>
2019-12-03 18:12:24 -0500

    255 issue

--- train.py
+++ train.py
@@ -29,10 +29,9 @@ from tqdm import tqdm
 
 use_cuda = torch.cuda.is_available()
 device = torch.device('cuda:0' if use_cuda else 'cpu')
-
 def normalize(x):
-    mean = [0.406, 0.485,0.456]
-    std = [0.225, 0.229,0.224]
+    mean = [0.485,0.456, 0.406]
+    std = [0.229,0.224, 0.225]
     mean = torch.Tensor(mean).reshape(1, -1, 1, 1)
     std = torch.Tensor(std).reshape(1, -1, 1, 1)
     x = (x - mean) / std
@@ -52,16 +51,21 @@ def gram(input):
 
 def train():
     epochs = 2
-    lr = 10e-3
-    content_weight = 1e-5
-    style_weight = 5e-10
+    lr = 1e-3
+    content_weight = 1e5
+    style_weight = 1e10
     batch_size = 4
     data_path = 'COCO/'
+    image_normalize = torchvision.transforms.Normalize(
+            mean=[0.485,0.456, 0.406],
+            std = [0.229,0.224, 0.225]
+        ) 
     transform=torchvision.transforms.Compose(
         [torchvision.transforms.Resize(256, 256),
         torchvision.transforms.CenterCrop((256, 256)),
         torchvision.transforms.ToTensor(),
-        ])
+        image_normalize
+    ])
     train_dataset = torchvision.datasets.ImageFolder(
         root=data_path,
         transform=transform
@@ -80,38 +84,45 @@ def train():
 
     vgg = Vgg16(requires_grad=False).to(device)
 
+    # compute style features and gram matrix
     style = Image.open("rickmorty-style.jpg")
     style = style.resize((256, 256))
-    style = transform(style).to(device)
-    normalized_style = normalize(style.reshape(1, 3, style.shape[1], style.shape[2]))
-    normalized_style = (normalized_style - torch.min(normalized_style))
-    normalized_style = normalized_style / torch.max(normalized_style)
-
-    normalized_style = normalized_style.repeat(batch_size, 1, 1, 1).to(device)
+    normalized_style = transform(style)
+    normalized_style = normalized_style.reshape(1, 3, normalized_style.shape[1], normalized_style.shape[2])
+    normalized_style = normalized_style.repeat(batch_size, 1, 1, 1)
+    normalized_style = normalized_style.to(device)
     features_style = vgg(normalized_style)
-
     gr_norm = [gram(ft) for ft in features_style]
     for e in tqdm(range(epochs)):
         loss = 0
+        agg_content_loss = 0
+        agg_style_loss = 0
         for idx, (example_data, _) in enumerate(train_loader):
             #tqdm.write(f'batch idx: {idx}')
-            plt.imshow(example_data[0].detach().cpu().numpy().transpose(1, 2, 0))
-            plt.show()
-            style_loss = 0
             optimizer.zero_grad()
-            output = gen(example_data)
-            normalized_output = normalize(output)
-            features_x = vgg(normalized_output)
-
-            feature_loss = content_weight * criterion(features_style.relu2_2, features_x.relu2_2)
-
-            for ft, gr_style in zip(features_x, gr_norm):
+            example_data = example_data * 255
+            output = gen(example_data.to(device))
+            normalized_output = output/255.0
+            features_output = vgg(normalized_output)
+            features_content = vgg(normalize(example_data))
+            content_loss = content_weight * criterion(features_output.relu2_2, features_content.relu2_2)
+            style_loss = 0
+            for ft, gr_style in zip(features_output, gr_norm):
                 gr = gram(ft)
                 style_loss += criterion(gr, gr_style)
             style_loss = style_weight * style_loss
-            loss = feature_loss + style_loss
-            tqdm.write(f'batch idx: {idx}, loss: {loss.item()}')
+            loss = content_loss + style_loss
             loss.backward()
+            agg_content_loss += content_loss.item()
+            agg_style_loss += style_loss.item()
+            if idx % 500 == 0:
+                mesg = "Epoch {}:\t[{}/{}]\tcontent: {:.6f}\tstyle: {:.6f}\ttotal: {:.6f}".format(
+                        e + 1, idx + 1, len(train_dataset),
+                        agg_content_loss / (idx + 1),
+                        agg_style_loss / (idx + 1),
+                        (agg_content_loss + agg_style_loss) / (idx + 1)
+                    )
+                tqdm.write(mesg)
             optimizer.step()
 
     torch.save(gen.state_dict(), f'gen.pt')
@@ -137,7 +148,7 @@ def test():
     test = test.reshape(1, 3, 256, 256)
     test = (test - torch.min(test))
     test = test / torch.max(test)
-    
+    test = test * 255
     stylized = gen(test)[0].detach().cpu().numpy()
     arr = stylized.transpose(1, 2, 0)
     new_arr = ((arr - arr.min()) * (1/(arr.max() - arr.min())))
@@ -148,5 +159,5 @@ def test():
     plt.show()
 
 if __name__ == '__main__':
-    #train()
-    test()
+    train()
+    #test()
